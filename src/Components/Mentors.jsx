f64a838e251cc6edf0c4c155ad46d959df9863d0
Joshim-Uddin <joshimuddin1831@gmail.com>
2025-05-14 11:45:42 +0600

    mentors section final

--- src/Components/Mentors.jsx
+++ src/Components/Mentors.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { FaLinkedin, FaLinkedinIn } from 'react-icons/fa';
-import { FiDownload } from 'react-icons/fi';
+import { FaLinkedin, FaQuoteLeft, FaRegStar, FaStar, FaStarHalf } from 'react-icons/fa';
 
 const Mentors = () => {
     const mentors=[{name:"Sachin Sharma", image:"https://res.cloudinary.com/dfxk2ofkn/image/upload/v1747101459/sachin_caszqy.jpg", position:"Managing Director"},{name:"Santosh Kumar Mishra", image:"https://res.cloudinary.com/dfxk2ofkn/image/upload/v1747101406/santosh_szqz9y.jpg", position:"Managing Director"},{name:"Prem Kumar", image:"https://res.cloudinary.com/dfxk2ofkn/image/upload/v1747101387/prem_uip2kk.jpg", position:"Mentorship"}]
     return (
-        <div className='pb-[119px] wrapper'>
+        <div className='pb-[84px] wrapper'>
                             <h6 className='font-poppins font-semibold leading-[26px] text-center tracking-[1.6px] text-[#1B99D4] uppercase'>DISCOVER OUR MENTORS</h6>
                             <h3 className='text-[32px] font-poppins font-semibold leading-[42px] text-center text-[#231F40] mt-[5px] max-w-[922px] mx-auto'>We have Industry Experts as Mentor who guide you with Career Planning & Interview Preparation</h3>
                         
@@ -29,7 +28,36 @@ const Mentors = () => {
                                 </div>
                            
                         
-                        <button className='bluebtn rounded-[4px] flex items-center gap-[5px] mx-auto mt-[107px] cursor-pointer text-lg leading-6 font-poppins font-semibold'>More Mentor Details</button>
+                        <button className='bluebtn rounded-[4px] flex items-center gap-[5px] mx-auto mt-[107px] mb-[119px] cursor-pointer text-lg leading-6 font-poppins font-semibold'>More Mentor Details</button>
+                        <div className='flex gap-[67px]'>
+                            <div className='max-w-[561px]'>
+                                <h3 className='font-inter font-bold text-5xl leading-16 tracking-[0.5px] text-[rgba(0,0,0,0.87)] mb-5 max-w-[550px] pr-[20px]'>Thousands of stories of growth</h3>
+                                <p className='font-poppins leading-8 text-[rgba(196,194,194,0.6)] max-w-[481.65px]'>Find out how our Learners transformed their careers after learning with us.</p>
+                            </div>
+                            <div className='max-w-[630px] px-[23px] py-[30px] shadow-[5px_3px_10px_5px_#767676] rounded-[10px] relative'>
+                                <FaQuoteLeft className='text-[#1B99D4] text-[43px]'/>
+                                <p className='font-inter font-medium leading-[26px] text-[#767676] my-[23px]'>“As a senior engineer I have specially reviewed the System Design content and live classes of My_Tutor and I found it more on the implementation side instead of theoretical concepts which helps to work in real-time environment in the organization. I would recommend My_Tutor course to get expertise in System Design.”</p>
+                                <div className=''>
+                                    <div className='flex items-center gap-[13px]'>
+                                        <img src={mentors[2].image} alt={`${mentors[2].name}'s image`} className='h-16 w-16 rounded-full'/>
+                                        <div>
+                                            <p className='mb-[15px] font-poppins font-semibold text-xl leading-5 text-[#1B99D4]'>{mentors[2].name}</p>
+                                            <div className='text-[#FFBA49] flex'>
+                                                <FaStar />
+                                                 <FaStar />
+                                                  <FaStar />
+                                                   <FaStar /><FaRegStar />
+                                            </div>
+                                           
+                                        </div>
+                                    </div>
+                                     <a href="#" className='flex items-center gap-[7.52px] font-poppins text-[13px] text-[#006DD9] absolute bottom-[33px] right-[23px]'>
+                                            <FaLinkedin className='text-[18.05px]'/> <span>View Profile</span>
+                                        </a>
+                                </div>
+
+                            </div>
+                        </div>
                             
                 </div>
     );
